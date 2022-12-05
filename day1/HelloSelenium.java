69ce9938b3e613e5f412b7e7ec7053cde5af1098
shanmugapriya0107 <47690470+shanmugapriya0107@users.noreply.github.com>
2022-12-05 11:48:48 +0530

    Add files via upload
    Week1.day1 - Classwork and Assignment

---
+++ day1/HelloSelenium.java
@@ -0,0 +1,11 @@
+package week1.day1;
+
+public class HelloSelenium {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello TestLeaf");
+		System.err.println("Hello TestLeaf");//Just to print an error statement
+
+	}
+
+}
---
+++ day1/Maths.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class Maths {
+		public int addTwoNos(int a, int b)
+		{
+			return (a+b);
+		}
+		public int mulTwoNos(int a, int b)
+		{
+			return (a*b);
+		}
+		public int divTwoNos(int a, int b)
+		{
+			return (a/b);
+		}
+
+
+}
