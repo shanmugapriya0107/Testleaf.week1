604452b2740248dd039b5e1dfa96630183044a7e
shanmugapriya0107 <47690470+shanmugapriya0107@users.noreply.github.com>
2022-12-05 13:24:18 +0530

    Add files via upload
    week1.day1 - Classwork and Assignments

---
+++ day1/assignments/MobileMethod.java
@@ -0,0 +1,19 @@
+package week1.day1.assignments;
+
+public class MobileMethod {
+
+	public int mobileCost(int a)
+	{
+	return a;
+	}
+	public boolean isNew(boolean a, boolean b)
+	{
+		return a;
+	}
+	public char logo()
+	{
+		return 'R';
+
+	}
+
+}
---
+++ day1/classwork/HelloSelenium.java
@@ -0,0 +1,11 @@
+package week1.day1.classwork;
+
+public class HelloSelenium {
+
+	public static void main(String[] args) {
+		System.out.println("Hello TestLeaf");
+		System.err.println("Hello TestLeaf");//Just to print an error statement
+
+	}
+
+}
